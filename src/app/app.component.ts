5fe24f170c1b616d04d775ee355e4ff1899202f9
Milan Drndarevic <drndica54@gmail.com>
2022-06-12 20:57:33 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'wholesale-discount-calculator';
+  title = 'Wholesale Discount Calculator';
 }
